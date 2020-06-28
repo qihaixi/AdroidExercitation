a9f6c106388400272b4574768241135e0f3a5543
qihaixi <qihaixi0930@gamil.com>
2020-06-28 10:23:29 +0800

    整合了聊天界面，目前还差点击联系人进入聊天界面

--- app/src/main/java/com/example/adroidexercitation/chat/MessageActivity.java
+++ app/src/main/java/com/example/adroidexercitation/chat/MessageActivity.java
@@ -2,16 +2,28 @@ package com.example.adroidexercitation.chat;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
+
+import com.example.adroidexercitation.MainActivity;
 import com.example.adroidexercitation.R;
 import com.netease.nimlib.sdk.NIMClient;
 import com.netease.nimlib.sdk.Observer;
@@ -22,10 +34,23 @@ import com.netease.nimlib.sdk.msg.MsgServiceObserve;
 import com.netease.nimlib.sdk.msg.constant.SessionTypeEnum;
 import com.netease.nimlib.sdk.msg.model.IMMessage;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class MessageActivity extends Activity {
+    //返回
+    private ImageButton fanhui;
+    //相机
+    private ImageButton xiangji;
+    private ImageButton imgIcon;//显示拍照照片，暂时没用
+    private Uri imageUri;   //  通用资源标志符
+    public static final int TAKE_PHOTO =1;  //  TAKE_PHOTO来作为case处理图片的标识
+
     private ChatAdapter chatAdapter;
     private ListView lv_chat_dialog;
     private List<PersonChat> personChats = new ArrayList<>();
@@ -73,14 +98,19 @@ public class MessageActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.message_layout);
+        setContentView(R.layout.activity_main_message);
+        //初始化云信sdk
         NIMClient.getService(MsgServiceObserve.class)
                 .observeReceiveMessage(incomingMessageObserver, true);
+
         lv_chat_dialog = findViewById(R.id.lv_chat_dialog);
         Button btn_chat_message_send = findViewById(R.id.btn_chat_message_send);
         final EditText et_chat_message = findViewById(R.id.et_chat_message);
+
         chatAdapter = new ChatAdapter(this,personChats);
         lv_chat_dialog.setAdapter(chatAdapter);
+
+        //发送消息按钮实现
         btn_chat_message_send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -89,17 +119,83 @@ public class MessageActivity extends Activity {
                     return;
                 }
                 PersonChat personChat = new PersonChat();
+                //true代表自己发送
                 personChat.setMeSend(true);
+                //从输入框中得到发送内容
                 personChat.setChatMessage(et_chat_message.getText().toString());
                 personChats.add(personChat);
+                //刷新ListView
                 chatAdapter.notifyDataSetChanged();
                 handler.sendEmptyMessage(1);
+                //发送消息
                 sendText(et_chat_message.getText().toString());
+                //清空输入框
                 et_chat_message.setText("");
+            }
+        });
+
+        //聊天页面左上角返回功能
+        fanhui = findViewById(R.id.fanhui);
+        fanhui.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                //设置显式Intent实现从MessageActivity到MainActivity的跳转
+                Intent intent =  new Intent(MessageActivity.this,MainActivity.class);
+                startActivity(intent);//调用startActivity（）方法，启动SecondActivity
+            }
+        });
+
+        //聊天页面拍照功能
+        xiangji = (ImageButton)findViewById(R.id.xiangji);
+        xiangji.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //创建时间对象作为拍照照片文件命名，防止多张照片覆盖
+                SimpleDateFormat format = new SimpleDateFormat("yyyy年MM月dd日HH:mm:ss");
+                Date curDate = new Date(System.currentTimeMillis());
+                String str = format.format(curDate);
+                /**
+                 *          创建File对象，用于存储拍照后的照片
+                 *          第一个参数：  是这张照片存放在手机SD卡的对应关联缓存应用
+                 *          第二个参数：  这张图片的命名
+                 */
+                File outputImage = new File(getExternalCacheDir(),str+".jpg");
+                try {
+                    if (outputImage.exists()){          //  检查与File对象相连接的文件和目录是否存在于磁盘中
+                        outputImage.delete();           //  删除与File对象相连接的文件和目录
+                    }
+                    outputImage.createNewFile();        //  如果与File对象相连接的文件不存在，则创建一个空文件
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (Build.VERSION.SDK_INT >= 24){       //  如果运行设备的系统版本高于 Android7.0
+                    /**
+                     *          将File对象转换成一个封装过的Uri对象
+                     *          第一个参数：  要求传入Context参数
+                     *          第二个参数：  可以是任意唯一的字符串
+                     *          第三个参数：  刚刚创建的File对象
+                     */
+                    imageUri = FileProvider.getUriForFile(MessageActivity.this,"com.example.adroidexercitation.chat.fileprovider",outputImage);
+                }else{                  //  如果运行设备的系统版本低于 Android7.0
+                    //  将File对象转换成Uri对象，这个Uri对象表示着 str + ".jpg" 这张图片的本地真实路径
+                    imageUri = Uri.fromFile(outputImage);
+                }
+                /**
+                 *      启动相机程序
+                 */
+                //  将Intent的action指定为 拍照到指定目录 —— android.media.action.IMAGE_CAPTURE
+                Intent intent = new Intent("android.media.action.STILL_IMAGE_CAMERA");
+                //  指定图片的输出地址
+                intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+                /**
+                 *      在通过startActivityForResult()，来启动活动，因此拍完照后会有结果返回到 onActivityResult()方法中
+                 */
+                startActivityForResult(intent,TAKE_PHOTO);
             }
         });
     }
 
+    //第三方云信即时通信
     public void sendText(String text){
         String account = "429784048";
         SessionTypeEnum sessionType = SessionTypeEnum.P2P;
@@ -122,6 +218,27 @@ public class MessageActivity extends Activity {
         });
     }
 
+    //用来显示拍照图片，暂时没用
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode){
+            case TAKE_PHOTO:
+                if (resultCode == RESULT_OK){       //  当拍照成功后，会返回一个返回码，这个值为 -1 — RESULT_OK
+                    try{
+                        //  根据Uri找到这张照片的资源位置，将它解析成Bitmap对象，然后将把它设置到imageView中显示出来
+                        Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                        imgIcon.setImageBitmap(bitmap);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+        }
+    }
 
+//	private void initView() {
+//		xiangji = findViewById(R.id.xiangji);
+//		imgIcon = findViewById(R.id.imgIcon);
+//	}
 
 }
